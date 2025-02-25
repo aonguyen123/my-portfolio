b7b58b49ac118cd3872dbb355d41f8cc6281dae4
Ao Nguyen <aonguyen113@gmail.com>
2025-02-26 01:46:57 +0700

    add layout

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,16 +2,16 @@ import { CommonModule } from '@angular/common';
 import { AfterViewInit, Component } from '@angular/core';
 import { themeChange } from 'theme-change';
 import { DropdownThemesComponent } from './components/dropdown-themes/dropdown-themes.component';
+import { NavBarComponent } from './components/nav-bar/nav-bar.component';
+import { SearchFieldComponent } from './components/search-field/search-field.component';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
-  imports: [CommonModule, DropdownThemesComponent],
+  imports: [CommonModule, NavBarComponent],
 })
 export class AppComponent implements AfterViewInit {
-  title = 'an_daisy';
-
   ngAfterViewInit(): void {
     themeChange(false);
   }
