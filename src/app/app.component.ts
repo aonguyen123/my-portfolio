0708d354d79198660252a499b8208440c79a17ea
Ao Nguyen <aonguyen113@gmail.com>
2025-02-27 00:06:25 +0700

    test1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { CommonModule } from '@angular/common';
 import { AfterViewInit, Component } from '@angular/core';
 import { themeChange } from 'theme-change';
-// import { HeaderComponent } from './components/header/header.component';
+import { HeaderComponent } from './components/header/header.component';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
+  imports: [HeaderComponent],
 })
 export class AppComponent implements AfterViewInit {
   ngAfterViewInit(): void {
